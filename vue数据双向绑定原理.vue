aa51384f73c11ac3c674c39f3936ed4e409a2ac6
Xu xiaohui <xuxh0911@163.com>
2020-08-26 22:56:10 +0800

    第一次创建仓库

---
+++ vue数据双向绑定原理.vue
@@ -0,0 +1,27 @@
+<template>
+    <div>
+        <input type="text" v-model="word">
+        <p>{{word}}</p>
+        <button v-on:click="sayHi"></button>
+    </div>
+</template>
+
+<script>
+    export default {
+        var vm = new MVVM({
+            el:'#mvvm-app',
+            data:{
+                word:'hello world'
+            },
+            methods:{
+                sayHi:function(){
+                    this.word = 'Hi,everybody'
+                }
+            }
+        })
+    }
+</script>
+
+<style lang="stylus" rel="stylesheet/stylus">
+
+</style>
